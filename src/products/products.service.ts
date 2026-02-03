82b461d41a588189ea44cecaedbcbf175f2aa6cb
Magdiel Mora <m41k80@icloud.com>
2026-02-03 06:59:09 -0600

    feat: add product-image table and relations

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,122 +1,184 @@
-import { BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException, ParseUUIDPipe } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+  ParseUUIDPipe,
+} from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { Product } from './entities/product.entity';
 import { validate as isUUID } from 'uuid';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
+import { ProductImage } from './entities';
 
 @Injectable()
 export class ProductsService {
-
   private readonly logger = new Logger('ProductsService');
 
   constructor(
     @InjectRepository(Product)
     private readonly productRepository: Repository<Product>,
+
+    @InjectRepository(ProductImage)
+    private readonly productImageRepository: Repository<ProductImage>,
+
+    private readonly dataSource: DataSource,
   ) {}
 
-   async create(createProductDto: CreateProductDto) {
-
-    try{
-      const product = this.productRepository.create(createProductDto);
+  async create(createProductDto: CreateProductDto) {
+    try {
+      const { images = [], ...productDetails } = createProductDto;
+      const product = this.productRepository.create({
+        ...productDetails,
+        images: images.map((image) =>
+          this.productImageRepository.create({ url: image }),
+        ),
+      });
       await this.productRepository.save(product);
 
-      return product;
+      return { ...product, images };
     } catch (error) {
-
       this.handleDBExceptions(error);
     }
-    
   }
 
-  findAll(paginationDto: PaginationDto) {
-
+  async findAll(paginationDto: PaginationDto) {
     const { limit = 10, offset = 0 } = paginationDto;
 
-    return this.productRepository.find({
+    const products = await this.productRepository.find({
       take: limit,
-      skip: offset
+      skip: offset,
+      relations: {
+        images: true,
+      },
     });
+
+    return products.map((product) => ({
+      ...product,
+      images: product.images?.map((image) => image.url),
+    }));
   }
 
   async findOne(term: string) {
-
     let product: Product | null;
 
     if (isUUID(term)) {
-      product = await this.productRepository.findOne({ where: { id: term } });
+      product = await this.productRepository.findOneBy({ id: term });
     } else {
-      const queryBuilder = this.productRepository.createQueryBuilder();
+      const queryBuilder = this.productRepository.createQueryBuilder('prod');
       product = await queryBuilder
-      .where('title ILIKE :title or slug = :slug', { title: term, slug: term.toLowerCase() }).getOne();
+        .where('title ILIKE :title or slug = :slug', {
+          title: term,
+          slug: term.toLowerCase(),
+        })
+        .leftJoinAndSelect('prod.images', 'prodImages')
+        .getOne();
     }
 
-    // const product = await this.productRepository.findOneBy({ id });
+    
     if (!product) {
       throw new NotFoundException(`Product #${term} not found`);
     }
     return product;
   }
 
+  async findONePlain(term: string) {
+    const { images = [], ...rest } = await this.findOne(term);
+    return {
+      ...rest,
+      images: images?.map((image) => image.url),
+    };
+  }
+
   async update(id: string, updateProductDto: UpdateProductDto) {
+    const { images, ...toUpdate } = updateProductDto;
 
     const product = await this.productRepository.preload({
-      id: id,
-      ...updateProductDto
-  });
+      id,
+      ...toUpdate,
+    });
     if (!product) {
       throw new NotFoundException(`Product #${id} not found`);
     }
 
-    try{
-      await this.productRepository.save(product);
-      return `product with id ${product.id} updated successfully`;
-    } catch (error) {
+    //  Create query runner
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
 
+    try {
+      
+      if (images) {
+        await queryRunner.manager.delete(ProductImage, { product: { id } });
+      
+
+      product.images = images?.map((image) =>
+        this.productImageRepository.create({ url: image }),
+      );
+    } 
+
+
+      await queryRunner.manager.save(product);
+      await queryRunner.commitTransaction();
+      await queryRunner.release();
+
+      
+      return this.findONePlain(id);
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      await queryRunner.release();
       this.handleDBExceptions(error);
     }
-    
-
-    
   }
 
   async remove(id: string) {
-  const product = await this.findOne(id);
+    const product = await this.findOne(id);
 
+    if (!product) {
+      throw new BadRequestException(`Product #${id} not found`);
+    }
 
-  if (!product) {
-    throw new BadRequestException(`Product #${id} not found`);
+    await this.productRepository.remove(product);
+    return `product with id ${id} removed successfully`;
   }
-
-  await this.productRepository.remove(product);
-  return `product with id ${id} removed successfully`;
-}
-
-
-
 
   private handleDBExceptions(error: unknown) {
-  if (this.isPostgresError(error) && error.code === '23505') {
-    throw new BadRequestException(error.detail);
+    if (this.isPostgresError(error) && error.code === '23505') {
+      throw new BadRequestException(error.detail);
+    }
+
+    this.logger.error(error);
+    throw new InternalServerErrorException('Unexpected error, check the logs');
   }
 
-  this.logger.error(error);
-  throw new InternalServerErrorException(
-    'Unexpected error, check the logs',
-  );
-}
+  private isPostgresError(
+    error: unknown,
+  ): error is { code: string; detail?: string } {
+    return (
+      typeof error === 'object' &&
+      error !== null &&
+      'code' in error &&
+      typeof (error as any).code === 'string'
+    );
+  }
 
-private isPostgresError(
-  error: unknown,
-): error is { code: string; detail?: string } {
-  return (
-    typeof error === 'object' &&
-    error !== null &&
-    'code' in error &&
-    typeof (error as any).code === 'string'
-  );
-}
+
+  async deleteAllProducts() {
+    const query = this.productRepository.createQueryBuilder('product');
+
+    try {
+      return await query
+        .delete()
+        .where({})
+        .execute();
+    } catch (error) {
+      this.handleDBExceptions(error);
+    }
+  }
+
 
 }
