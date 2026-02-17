be91cfef79720a42207dcbe02ff1046914fb547f
Magdiel Mora <m41k80@icloud.com>
2026-02-17 13:17:39 -0600

    feat: add messagesWs-dto

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,16 +1,49 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { MessagesWsService } from './messages-ws.service';
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
+import { NewMessageDto } from './dtos/new-message.dto';
 
 @WebSocketGateway({cors: true})
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
+
+  @WebSocketServer() wss: Server;
   constructor(
     private readonly messagesWsService: MessagesWsService)
     {}
   handleConnection(client: Socket) {
-    console.log('Client connected', client.id);
+
+    this.messagesWsService.registerClient(client);
+
+    this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients());
+    
+    
+   
   }
   handleDisconnect(client: Socket) {
-    console.log('Client disconnected', client.id);
+    
+    this.messagesWsService.removeClient(client.id);
+    this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients());
+    
+  }
+
+  @SubscribeMessage('message-from-client')
+  handleMessageFromClient(client: Socket, payload: NewMessageDto) {
+    
+    // emit message to a client
+    // client.emit('message-from-server', {
+    //   fullName: 'soy yo',
+    //   message: payload.message || 'no-message!!'
+    // });
+
+    // emit message to all clients but not the sender
+    // client.broadcast.emit('message-from-server', {
+    //   fullName: 'soy yo',
+    //   message: payload.message || 'no-message!!'
+    // });
+
+    this.wss.emit('message-from-server', {
+      fullName: 'soy yo',
+      message: payload.message || 'no-message!!'
+    });
   }
 }
--- src/messages-ws/messages-ws.service.ts
+++ src/messages-ws/messages-ws.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { Socket } from 'socket.io';
+
+interface ConecctedClient {
+    [id: string]: Socket
+}
 
 @Injectable()
-export class MessagesWsService {}
+export class MessagesWsService {
+
+    private connectedClients: ConecctedClient = {};
+
+    registerClient(client: Socket) {
+        this.connectedClients[client.id] = client;
+    }
+    removeClient(clientId: string) {
+        delete this.connectedClients[clientId];
+    }
+
+    getConnectedClients(): string[] {
+        return Object.keys(this.connectedClients);
+    }
+}
